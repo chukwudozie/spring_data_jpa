34666dbd05e7aaa22e40a1f78b17d758a3ac65b1
chukwudozie <emkychuks062@gmail.com>
2022-05-01 16:43:37 +0100

    methods for find all books with paging

--- src/main/java/saha/code/spring_data_jpa/dao/BookDao.java
+++ src/main/java/saha/code/spring_data_jpa/dao/BookDao.java
@@ -1,10 +1,17 @@
 package saha.code.spring_data_jpa.dao;
 
+import org.springframework.data.domain.Pageable;
 import saha.code.spring_data_jpa.domain.Book;
 
 import java.util.List;
 
 public interface BookDao {
+
+    List<Book> findAllBooksSortByTitle(Pageable pageable);
+    List<Book> findAllBooks(Pageable pageable);
+    List<Book> findAllBooks(int pageSize, int offset);
+
+    List<Book> findAllBooks();
     Book getById(Long id);
 
     Book saveNewBook(Book book);
--- src/main/java/saha/code/spring_data_jpa/dao/BookDaoImpl.java
+++ src/main/java/saha/code/spring_data_jpa/dao/BookDaoImpl.java
@@ -1,5 +1,9 @@
 package saha.code.spring_data_jpa.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 import saha.code.spring_data_jpa.domain.Book;
 import saha.code.spring_data_jpa.repositories.BookRepository;
@@ -15,6 +19,34 @@ public class BookDaoImpl implements BookDao{
 
     public BookDaoImpl(BookRepository bookRepository) {
         this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public List<Book> findAllBooksSortByTitle(Pageable pageable) {
+        Page<Book> bookPage = bookRepository.findAll(pageable);
+        return bookPage.getContent();
+    }
+
+    @Override
+    public List<Book> findAllBooks(Pageable pageable) {
+        return bookRepository.findAll(pageable).getContent();
+    }
+
+    @Override
+    public List<Book> findAllBooks(int pageSize, int offset) {
+
+        Pageable pageable = PageRequest.ofSize(pageSize);
+        if(offset > 0){
+            pageable = pageable.withPage(offset/pageSize);
+        } else {
+            pageable = pageable.withPage(0);
+        }
+        return this.findAllBooks(pageable);
+    }
+
+    @Override
+    public List<Book> findAllBooks() {
+        return bookRepository.findAll();
     }
 
     @Override
