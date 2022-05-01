49a9b59649ab4c98c508d22a626640a04dc05459
chukwudozie <emkychuks062@gmail.com>
2022-05-01 16:44:09 +0100

    data migration sql files

---
+++ src/main/resources/db/migration/V2__insert_books.sql
@@ -0,0 +1,46 @@
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Marcobehlar',
+                                                             'Spring Framework',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Bach Stephens',
+                                                             'Rabbit-Mq',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Marcobehlar',
+                                                             'Spring Framework 2nd Edition',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Bach Stephens',
+                                                             'Rabbit-Mq 2nd Edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Marcobehlar',
+                                                             'Spring Framework 3rd Edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Bach Stephens',
+                                                             'Rabbit-Mq 3rd Edition',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Oreilly James',
+                                                             'Java for Dummies Framework',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Emeka Chukwudozie',
+                                                             'Kubernetes-Cluster',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Oreilly James',
+                                                             'Java for Dummies Framework 2nd edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Emeka Chukwudozie',
+                                                             'Kubernetes-Cluster 2nd edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Oreilly James',
+                                                             'Java for Dummies Framework 3rd edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Emeka Chukwudozie',
+                                                             'Kubernetes-Cluster 3rd edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Oreilly James',
+                                                             'Java for Dummies Framework 3rd edition',(select id from author where first_name = 'Craig' and last_name = 'Walls') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-0321125220', 'Emeka Chukwudozie',
+                                                             'Kubernetes-Cluster 3rd edition',(select id from author where first_name = 'Eric' and last_name = 'Evans') );
+
+insert into book (isbn, publisher, title, author_id) values ('978-1617294950', 'Marcobehlar',
+                                                             'Spring Framework',(select id from author where first_name = 'Robert' and last_name = 'Martin') );
+
+
